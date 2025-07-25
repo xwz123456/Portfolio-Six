e69e3934deeb4bbd2e229c51abb7b5df589de4e8
xwztrain <123456789@qq.com>
2025-07-25 07:03:28 +0000

    SIX-backen-006 : Add two api for user to query total asset history info and total profit history info

--- routes/assetRoutes.js
+++ routes/assetRoutes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const assetController = require('../controllers/assetController');
 
 // Define asset-related routes
-router.get(':userId', assetController.getAssetsByUserId); // New route to fetch assets by user ID
+router.get('/findAllAssets/:userId', assetController.getAssetsByUserId); // New route to fetch assets by user ID
+
+// get total assets history info by user ID
+router.get('/totalAssetsHistoryInfo/:userId', assetController.getTotalAssetsHistoryInfoByUserId); // New route to fetch total assets history by user ID
+
+// get total profit history info by user ID
+router.get('/totalProfitHistoryInfo/:userId', assetController.getTotalProfitHistoryInfoByUserId); // Fix typo here
 
 module.exports = router;
