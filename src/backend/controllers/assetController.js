f112ff473294d28dd0061a10ced8874d042f5b9f
zykzhang <913307071@qq.com>
2025-07-28 07:48:10 +0000

    update_asset

--- src/backend/controllers/assetController.js
+++ src/backend/controllers/assetController.js
@@ -1,5 +1,5 @@
 const db = require('../models/db');
-// Get all assets for a specific user by user ID
+// 中下ALL
 exports.getAssetsByUserId = async (req, res) => {
   const {userId} = req.params;
   try {
@@ -13,22 +13,8 @@ exports.getAssetsByUserId = async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch assets', error: err.message });
   }
 };
-// getAllAssets
-exports.getAllAssets = async (req, res) => {
-  try {
-    const [rows] = await db.query('SELECT * FROM user_assets');
-    res.status(200).json(
-        {
-          success: true,
-          data: rows
-        });
-  } catch (err) {
-    console.error('Error fetching stock:', err);
-    res.status(500).json({message: 'Failed to fetch stocks', error: err.message});
-  }
-};
 // getAssetsByAsset_type
-// 获取某用户&某资产类别的数据
+// 获取某用户&某资产类别的数据(中下选择)
 exports.getAssetsByUserIdAndType = async (req, res) => {
   const { userId, assetType } = req.params;
   try {
@@ -49,7 +35,7 @@ exports.getAssetsByUserIdAndType = async (req, res) => {
     });
   }
 };
-// 计算某用户所有资产类型分别的总收益率
+// 计算某用户所有资产类型分别的总收益和总收益率(右上四个框)
 exports.getUserAssetTypeProfitRates = async (req, res) => {
   const { userId } = req.params;
   try {
@@ -87,7 +73,7 @@ exports.getUserAssetTypeProfitRates = async (req, res) => {
     });
   }
 };
-//获取用户时间线上的总资产和总收益
+//获取用户时间线上的总资产和总收益(中上折现&柱状图数据)
 exports.getUserTotalAssetHistory = async (req, res) => {
   const { userId } = req.params;
   try {
@@ -110,39 +96,4 @@ exports.getUserTotalAssetHistory = async (req, res) => {
 
 
 
-// //计算某用户某特定类型资产的总收益率
-// exports.getProfitRate = async (req, res) => {
-//   const { userId, assetType } = req.params;
-//   try {
-//     const [rows] = await db.query(
-//         'SELECT * FROM user_assets WHERE user_id = ? AND asset_type = ?',
-//         [userId, assetType]
-//     );
-//
-//     // 计算总投入和总现值
-//     let totalCost = 0;
-//     let totalCurrent = 0;
-//     rows.forEach(asset => {
-//       totalCost += Number(asset.purchase_price) * Number(asset.quantity);
-//       totalCurrent += Number(asset.current_price) * Number(asset.quantity);
-//     });
-//
-//     // 计算整体收益率
-//     let overallReturn = null;
-//     if (totalCost > 0) {
-//       overallReturn = (totalCurrent - totalCost) / totalCost;
-//     }
-//
-//     res.status(200).json({
-//       success: true,
-//       data: overallReturn*100 // 返回整体收益率
-//       // 例如 0.15 表示 15%
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       success: false,
-//       message: '查询失败',
-//       error: err.message
-//     });
-//   }
-// };
+
--- src/backend/routes/assetRoute.js
+++ src/backend/routes/assetRoute.js
@@ -4,11 +4,9 @@ const assetController = require('../controllers/assetController.js');
 const path = require('path');
 // 定义路由，调用对应的控制器方法
 
-router.get('/asset/:userId', assetController.getAssetsByUserId); // 获取某用户资产数据
-router.get('/asset', assetController.getAllAssets); // 获取所有资产数据
-router.get('/asset/:userId/:assetType', assetController.getAssetsByUserIdAndType); // 获取某用户某资产类别数据
-// router.get('/rate/:userId/:assetType', assetController.getProfitRate); // 计算某用户某类型资产的总收益率
-router.get('/rate/:userId', assetController.getUserAssetTypeProfitRates); // 计算某用户所有资产类型分别的总收益率
-router.get('/history/:userId', assetController.getUserTotalAssetHistory); // 获取某用户所有资产的历史数据
+router.get('/asset/:userId', assetController.getAssetsByUserId); // 获取某用户资产数据(中下ALL)
+router.get('/asset/:userId/:assetType', assetController.getAssetsByUserIdAndType); // 获取某用户某资产类别数据(中下选择)
+router.get('/rate/:userId', assetController.getUserAssetTypeProfitRates); // 计算某用户所有资产类型分别的总收益和总收益率(右上四个框)
+router.get('/history/:userId', assetController.getUserTotalAssetHistory); // 获取某用户所有资产的历史数据(中上折现&柱状图数据)
 
 module.exports = router;
