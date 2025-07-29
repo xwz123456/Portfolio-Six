e41cbfb2f30e1d506ff9ca84c0cef2859a4af484
xwztrain <123456789@qq.com>
2025-07-29 06:51:02 +0000

    SIX-backend-009 ： Add eslint in backend

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -1,20 +1,20 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Portfolio Management API',
-            version: '1.0.0',
-            description: 'API documentation for the Portfolio Management project',
-        },
-        servers: [
-            {
-                url: 'http://localhost:5000', 
-            },
-        ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Portfolio Management API',
+      version: '1.0.0',
+      description: 'API documentation for the Portfolio Management project',
     },
-    apis: ['./routes/*.js', './controllers/*.js'], // Ensure these paths are correct
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js', './controllers/*.js'], // Ensure these paths are correct
 };
 
 const swaggerSpec = swaggerJsdoc(options);
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true, // Use semicolons
+  singleQuote: true, // Use single quotes
+  trailingComma: 'es5', // Add trailing commas where valid in ES5
+  tabWidth: 2, // Indent with 2 spaces
+  useTabs: false, // Do not use tabs for indentation
+  printWidth: 80, // Maximum line length is 80 characters
+  bracketSpacing: true, // Add spaces between brackets in object literals
+  arrowParens: 'avoid' // Omit parentheses when arrow function has one parameter
+};
